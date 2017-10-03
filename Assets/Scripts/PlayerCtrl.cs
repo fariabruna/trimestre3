4f1aa276354d3eb951d5bbe36b804dad1d1e0493
Bruna Faria <fariabruna71@gmail.com>
2017-10-03 11:37:21 -0300

    Fazendo o player pular

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -6,6 +6,8 @@ public class PlayerCtrl : MonoBehaviour {
 
     public float horizontalSpeed = 10f;
 
+	public float jumpSpeed = 600f;
+
     Rigidbody2D rb;
 
     // Use this for initialization
@@ -24,6 +26,9 @@ public class PlayerCtrl : MonoBehaviour {
         else {
             StopMoving();
         }
+		if(Input.GetButtonDown("Jump")){
+            Jump();
+        }
     }
 
     void MoveHorizontal(float speed) {
@@ -32,5 +37,9 @@ public class PlayerCtrl : MonoBehaviour {
 
     void StopMoving() {
         rb.velocity = new Vector2(0f, rb.velocity.y);
+    }
+
+	void Jump() {
+        rb.AddForce(new Vector2(0f, jumpSpeed));
     }
 }
