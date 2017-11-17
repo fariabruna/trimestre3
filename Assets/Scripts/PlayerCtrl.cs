7564e23a6e4b7bdb6b5a45cee72284445ef3f76b
Bruna Faria <fariabruna71@gmail.com>
2017-11-17 20:09:58 -0200

    Correco: teste de moeda em playerCtrl

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -130,4 +130,10 @@ public class PlayerCtrl : MonoBehaviour {
             isJumping = false;
         }
     }
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("Coin")){
+            Destroy(other.gameObject);
+
+        }
+    }
 }
