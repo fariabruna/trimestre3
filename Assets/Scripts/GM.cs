707822f53fe7060b35795428b42aa4c643376c37
Bruna Faria <fariabruna71@gmail.com>
2017-11-25 16:35:51 -0200

    Contando vidas

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -61,11 +61,16 @@ public class GM : MonoBehaviour {
 
     void DisplayHudData(){
         ui.hud.txtcoinCount.text = "x " + data.coinCount;
+        ui.hud.txtLifeCount.text = "x " + data.lifeCount;
         ui.hud.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
     }
 
     public void IncrementCoinCount(){
         data.coinCount++;
+    }
+
+    public void DecrementLives(){
+        data.lifeCount--;
     }
 
     public void RespawnPlayer() {
@@ -75,7 +80,13 @@ public class GM : MonoBehaviour {
 	public void KillPlayer(){
 		if (player != null){
 			Destroy(player.gameObject);
-			Invoke("RespawnPlayer", timeToRespawn);
+            DecrementLives();
+            if (data.lifeCount > 0){
+                Invoke("RespawnPlayer", timeToRespawn);
+            }
+            else{
+                GameOver();
+            }
 		}	
 	}
 
